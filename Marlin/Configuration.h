a3a1059ffd9371cd5092b68455c95ae08bad5df4
cdeitrich00 <56947221+cdeitrich00@users.noreply.github.com>
2022-11-20 20:31:47 -0500

    rename printer

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -60,7 +60,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(chad, neverender skr e3 mini v3 config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(chad, enderque skr e3 mini v3 config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
